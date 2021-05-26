b4a7589d8f2c0fec491f9e3dd4869cdf912a4359
sultansingh7073 <sultankhilari7073@gmail.com>
2021-05-26 08:39:46 +0530

    work done by sultan

--- github_follower_app/lib/views/home.dart
+++ github_follower_app/lib/views/home.dart
@@ -24,6 +24,7 @@ class _HomePageState extends State<HomePage> {
           children: [
             Container(
               height: 100,
+               // height: 100,
               width: 100,
               child: CircleAvatar(
                 backgroundColor: Colors.black,
